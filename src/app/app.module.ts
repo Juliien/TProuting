04e42c3a64eedd1373aaa3533d7a4392be7d7d62
Juliien <julien1105@outlook.fr>
2020-06-26 21:23:13 +0200

    tpRouting

---
+++ src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'servers', component: ServersComponent,
+    children: [{  path: ':id', component: ServerComponent },
+      {  path: ':id/:edit', component: EditServerComponent }]},
+  { path: 'users', component: UsersComponent,
+    children : [{  path: ':id/:name', component: UserComponent },
+  ]},
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  ];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [ServersService],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
