f91e45dc061cb6c0cf1ffafd63a359f4a6c8da14
Juliien <julien1105@outlook.fr>
2021-01-21 02:48:54 +0100

    hotfix

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, convertToParamMap, ParamMap} from '@angular/router';
+import {ActivatedRoute, ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-user',
